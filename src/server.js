d387140e8371732d323111eec79dad1c9e5c9d5a
aarancis <pG0W3M6I8l>
2020-09-15 17:51:14 -0500

    fixing package

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname+'/dist/appsteps'));
 app.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/appsteps/index.html'));
 });
 
 app.listen(process.env.PORT || 7070);
